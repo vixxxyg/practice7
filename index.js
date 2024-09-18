e670b55ebe1f56a60b50c46ba1ffd7a7b010455d
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-09-18 23:14:18 +0200

    2nd commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let coincidencia = 0;
 function inicio() {
   document.querySelector(".boton").onclick = empezar;
   dibujarMonedas();
+  vaciar();
 }
 
 function empezar() {
@@ -14,6 +15,7 @@ function empezar() {
   if (valor >= 2 && valor <= 9) {
     crearCuadros();
   } else {
+    vaciar();
   }
 }
 
@@ -30,13 +32,15 @@ function crearCuadros() {
       );
   }
   if (coincidencia > 0) {
-    var mensaje = `Se ha producido ${coincidencia} coincidencias y has ganado ${coincidencia} monedas`;
+    var mensaje = `Se ha producido ${coincidencia} coincidencia${plural(coincidencia)} y has ganado ${coincidencia} moneda${plural(coincidencia)}`;
     monedas += coincidencia;
   } else {
     var mensaje = `No se ha producido ninguna coincidencia y has perdido ${valor} monedas`;
     monedas -= valor;
   }
+  document.querySelector(".informacion").innerHTML = mensaje;
   dibujarMonedas();
+  vaciar();
 }
 
 function comprobar(azar) {
@@ -50,8 +54,21 @@ function comprobar(azar) {
 
 function dibujarMonedas() {
   let m = document.querySelector(".monedas");
-  m.innerHTML = `<div>Monedas: <span class = "grande">${monedas}</span></div>`;
+  m.innerHTML = `<div class = "saldo">Monedas: <span class = "grande">${monedas}</span></div>`;
   for (let k = 0; k < monedas; k++) {
     m.insertAdjacentHTML("beforeend", `<img src = "img/coin.png">`);
   }
 }
+
+function vaciar() {
+  document.querySelector("#campo").value = "";
+  document.querySelector("#campo").focus();
+}
+
+function plural (cantidad) {
+  if (cantidad !=1){
+    return "s";
+  } else {
+    return "";
+  }
+};
