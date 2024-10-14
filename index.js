05194d537043baf16f1a13bfe1937ab98bd1c342
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-10-14 13:56:44 +0200

    1st

--- index.js
+++ index.js
@@ -1,74 +1,23 @@
 window.onload = inicio;
-let valor;
-let monedas = 5;
-let coincidencia = 0;
+let rojo = `<img src="/img/rocket-7757105_640.png" alt="">`;
+let azul = `<img src="/img/logo-2582748_640.png">`;
+let caja;
 
-function inicio() {
-  document.querySelector(".boton").onclick = empezar;
-  dibujarMonedas();
-  vaciar();
+function inicio(){
+    caja = document.querySelector("#caja");
+    document.querySelectorAll('button')[0].onclick = unoRojo;
+    document.querySelectorAll("button")[1].onclick = cuatroAzules;
 }
 
-function empezar() {
-    coincidencia = 0;
-  valor = document.querySelector("#campo").value;
-  if (valor >= 2 && valor <= 9) {
-    crearCuadros();
-  } else {
-    vaciar();
-  }
+function unoRojo(){
+    caja.insertAdjacentHTML("beforeend", rojo);
+
 }
 
-function crearCuadros() {
-  document.querySelector(".contenido").innerHTML = "";
-  for (let k = 0; k < valor; k++) {
-    let azar = Math.ceil(Math.random() * valor);
-    let estilo = comprobar(azar);
-    document
-      .querySelector(".contenido")
-      .insertAdjacentHTML(
-        "beforeend",
-        `<div class = "cuadro ${estilo}">${azar}</div>`
-      );
-  }
-  if (coincidencia > 0) {
-    var mensaje = `Se ha producido ${coincidencia} coincidencia${plural(coincidencia)} y has ganado ${coincidencia} moneda${plural(coincidencia)}`;
-    monedas += coincidencia;
-  } else {
-    var mensaje = `No se ha producido ninguna coincidencia y has perdido ${valor} monedas`;
-    monedas -= valor;
-  }
-  document.querySelector(".informacion").innerHTML = mensaje;
-  dibujarMonedas();
-  vaciar();
-}
+function cuatroAzules(){
+caja.insertAdjacentHTML("beforeend", azul);
+caja.insertAdjacentHTML("beforeend", azul);
+caja.insertAdjacentHTML("beforeend", azul);
+caja.insertAdjacentHTML("beforeend", azul);
 
-function comprobar(azar) {
-  if (azar == valor) {
-    coincidencia++;
-    return "verde";
-  } else {
-    return "rojo";
-  }
 }
-
-function dibujarMonedas() {
-  let m = document.querySelector(".monedas");
-  m.innerHTML = `<div class = "saldo">Monedas: <span class = "grande">${monedas}</span></div>`;
-  for (let k = 0; k < monedas; k++) {
-    m.insertAdjacentHTML("beforeend", `<img src = "img/coin.png">`);
-  }
-}
-
-function vaciar() {
-  document.querySelector("#campo").value = "";
-  document.querySelector("#campo").focus();
-}
-
-function plural (cantidad) {
-  if (cantidad !=1){
-    return "s";
-  } else {
-    return "";
-  }
-};
